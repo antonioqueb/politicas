853cb9b413a3bbdb77d954b136df33f07a3dd7c2
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-03 22:53:44 -0600

    Up

--- models/policy_category.py
+++ models/policy_category.py
@@ -3,9 +3,6 @@ from odoo import api, fields, models
 class CompanyPolicyCategory(models.Model):
     _name = 'company.policy.category'
     _description = 'Categorías de Políticas (con jerarquía)'
-    _parent_name = 'parent_id'
-    _parent_store = True
-    _order = 'parent_left'
 
     name = fields.Char(
         string='Nombre de la Categoría',
@@ -21,10 +18,6 @@ class CompanyPolicyCategory(models.Model):
         'parent_id',
         string='Subcategorías'
     )
-
-    # Campos auxiliares para guardar la jerarquía
-    parent_left = fields.Integer(index=True)
-    parent_right = fields.Integer(index=True)
 
     # Campo calculado para ver la jerarquía completa en nombre
     complete_name = fields.Char(
@@ -44,13 +37,3 @@ class CompanyPolicyCategory(models.Model):
                 record.complete_name = f"{record.parent_id.complete_name} / {record.name}"
             else:
                 record.complete_name = record.name
-
-    def name_get(self):
-        """
-        Para que en los Many2one se muestre la ruta completa, 
-        en lugar de solo el nombre de la subcategoría.
-        """
-        result = []
-        for record in self:
-            result.append((record.id, record.complete_name))
-        return result
